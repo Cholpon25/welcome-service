4ced16ffc6a72d1dd7a6aecf17b842beeeda2af9
EC2 Default User <ec2-user@ip-172-31-38-99.us-east-2.compute.internal>
2025-06-06 20:39:41 +0000

    Add fixed welcomescript.sh

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-echo "Hello 312 Stadent!"
-echo "Inter Your Name Please:"
+
+
+echo "Hello 312 Student!"
+echo "Enter Your Name Please:"
 read name
-echo "$names, welcome to 313 School"
+echo "$name, Welcome to 312 School"
+
+echo "Hello 313 Student!"
+echo "Enter Your Name Please:"
+read name2
+echo "$name2, Welcome to 313 School"
+
+
+echo "Hello 312 Student!"
+echo "Enter your name please:"
+read name
+<<<<<<< HEAD
+echo "$names, welcome to 312 School"
+
+=======
+echo "$name, welcome to 312 School"
+>>>>>>> 23adee3 (Fix welcomescript.sh and prepare for final push)
